c1c61127ba8b2a302c40f999da5ee3ddd028c110
shimjaehyung <shim5776@naver.com>
2023-01-20 23:07:01 +0900

    123

--- detect_pub.py
+++ detect_pub.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 from threading import Thread
-from unittest import result
 
 import rospy
 from sensor_msgs.msg import Image as msgImage
@@ -31,8 +30,8 @@ class LoadTopic:  # for inference
 
         self.bridge = CvBridge()
         self.sub_rgb=rospy.Subscriber(rgb_image_topic,msgImage,self.imageRGBCallback)
-        self.pub_yolo = rospy.Publisher('/dsr_tray/yolo_result', msgYolo, queue_size=5)
-
+        self.pub_yolo_result = rospy.Publisher('/dsr_tray/yolo_result', msgYolo, queue_size=5)
+        # self.pub_yolo_image = rospy.Publisher('/dsr_tray/yolo_image', msgImage, queue_size=3)
         self.yolo=msgYolo()
 
     def __iter__(self):
@@ -41,15 +40,14 @@ class LoadTopic:  # for inference
 
     def __next__(self):
         self.count += 1
-        if cv2.waitKey(1) == ord('q'):  # q to quit
-            cv2.destroyAllWindows()
+        if rospy.is_shutdown():
             raise StopIteration
 
         # Read frame
-        timeout=1
+        timeout=7
         t1=time.time()
         while self.img is None and time.time()-t1<timeout:
-            rospy.logwarn('no image topic')
+            # rospy.logwarn('no image topic')
             rospy.sleep(1)
         if self.img is None:
             rospy.logwarn('timeout!!')
@@ -74,6 +72,8 @@ class LoadTopic:  # for inference
     def imageRGBCallback(self,Image): 
         try:
             cv_rgbimg=self.bridge.imgmsg_to_cv2(Image,"bgr8")
+            # print(cv_rgbimg.shape)
+            # self.img=cv2.resize(cv_rgbimg, (0,0), fx=16/9, fy=16/9)
             self.img=cv_rgbimg
         except CvBridgeError as e:
             print(e)
@@ -90,8 +90,9 @@ def detect(weights='', imgsz=640, conf_thres=0.4, iou_thres=0.5, dev='', agnosti
     half = device.type != 'cpu'  # half precision only supported on CUDA
 
     if weights == '':
-        weight_path=os.path.dirname(os.path.abspath(__file__))+ '/runs/exp11_yolov4-csp-results/weights/' + 'best_yolov4-csp-results.pt'
-    
+        # weight_path=os.path.dirname(os.path.abspath(__file__))+ '/runs/exp22_yolov4-csp-results/weights/' + 'best.pt'#'best_yolov4-csp-results.pt'
+        weight_path=os.path.dirname(os.path.abspath(__file__))+ '/runs/exp22_yolov4-csp-results/weights/' + 'best_yolov4-csp-results.pt'
+        
     # Load model
     model = attempt_load(weight_path, map_location=device)  # load FP32 model
     imgsz = check_img_size(imgsz, s=model.stride.max())  # check img_size
@@ -112,8 +113,6 @@ def detect(weights='', imgsz=640, conf_thres=0.4, iou_thres=0.5, dev='', agnosti
     with torch.no_grad():
         for path, img, im0s, vid_cap in dataset:
             dataset.yolo.image=dataset.bridge.cv2_to_imgmsg(im0s)
-            # cv2.imshow('im0', im0s)
-            # cv2.waitKey(0)
             # rospy.logwarn(torch.cuda.memory_allocated()/1024**2)
             img = torch.from_numpy(img).to(device)
             img = img.half() if half else img.float()  # uint8 to fp16/32
@@ -157,14 +156,17 @@ def detect(weights='', imgsz=640, conf_thres=0.4, iou_thres=0.5, dev='', agnosti
                         detection_result += ('%g ' * 5 + '\n') % (cls, *xyxy) # label format
                         label = '%s' % (names[int(cls)])
                         plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=2, rect_thickness=1)
-                    rospy.loginfo(detection_result)
+                    rospy.loginfo("\n"+detection_result)
                     
                     result_str = msgString()
                     result_str.data= detection_result
                     dataset.yolo.string=result_str
                     
-                    dataset.pub_yolo.publish(dataset.yolo) #publish original image to find handle
-            cv2.imshow('detection', im0)
+                    
+                    dataset.pub_yolo_result.publish(dataset.yolo) #publish original image to find handle
+            
+            # dataset.pub_yolo_image.publish(dataset.bridge.cv2_to_imgmsg(im0, encoding='bgr8'))
+            cv2.imshow('detection', cv2.resize(im0, (0,0), fx=0.5, fy=0.5))
             cv2.waitKey(1)
 
     # Print time (inference + NMS)
@@ -174,14 +176,14 @@ def detect(weights='', imgsz=640, conf_thres=0.4, iou_thres=0.5, dev='', agnosti
 
 def main():
     global dataset
-    rgb_image_topic= "/camera/color/image_raw"
-    # rgb_image_topic= "/usb_cam/image_raw"
-    # rgb_image_topic= "/eye_to_hand_cam/color/image_raw"
+    rgb_image_topic= "/global_camera/color/image_raw"
+    # rgb_image_topic= "/camera/color/image_raw"
     dataset = LoadTopic(rgb_image_topic)
     try:
         rospy.spin()
     except KeyboardInterrupt:
         print("Shutting down")
+        del dataset
     cv2.destroyAllWindows()
 
 #####################
@@ -190,11 +192,10 @@ global dataset
 if __name__=='__main__':
     node_name = os.path.basename(sys.argv[0]).split('.')[0]
     rospy.init_node(node_name)
-    rospy.loginfo("press q to quit")
     
     th1 = Thread(target=main)
     rospy.sleep(3) # wait until node starts
-    th2 = Thread(target=detect, args=('', 416, 0.4, 0.5, '', False, False), daemon=True)
+    th2 = Thread(target=detect, args=('', 480, 0.4, 0.5, '', False, False), daemon=True)
 
     th1.start()
     th2.start()
